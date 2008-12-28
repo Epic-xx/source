1fc6757b50cc305e0573d5b2a92f6b69f4a25c52
khemical <@srhclan.com>
2008-12-28 11:23:53 -0500

    first commit

---
+++ run_hammer.bat
@@ -0,0 +1,2 @@
+@"c:\program files\steam\steamapps\xurah\sourcesdk\bin\hammer.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
---
+++ run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"c:\program files\steam\steamapps\xurah\sourcesdk\bin\studiomdl.exe" -game "c:\program files\steam\steamapps\SourceMods\sourcecode" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
