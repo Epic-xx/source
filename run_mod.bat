f9efd14c47b01d07d326948cb30f0ea615e768c6
khemical <@srhclan.com>
2008-12-28 12:42:45 -0500

    deleted:    README 	modified:   run_mod.bat 	modified:   src/Game_HL2MP-2005.sln 	new file:   src/Game_HL2MP-2005.suo 	modified:   src/game/client/client_hl2mp-2005.vcproj 	modified:   src/game/server/server_hl2mp-2005.vcproj 	src/game/client/Debug_hl2mp/ 	src/game/client/client_hl2mp-2005.vcproj.HAX-04BDDE92B8F.EpicLawlz.user 	src/game/server/Debug_hl2mp/ 	src/game/server/server_hl2mp-2005.vcproj.HAX-04BDDE92B8F.EpicLawlz.user

--- run_mod.bat
+++ run_mod.bat
@@ -1,1 +1,1 @@
-@"c:\program files\steam\steamapps\xurah\source sdk base 2007\hl2.exe" -dev -game "c:\program files\steam\steamapps\SourceMods\sourcecode" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"c:\program files\steam\steamapps\xurah\source sdk base\hl2.exe" -dev -game "c:\program files\steam\steamapps\SourceMods\sourcecode" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
